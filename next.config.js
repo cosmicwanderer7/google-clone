fc45572c485af8242711532228c0aaa49b1cd053
Prithvijeet <65147258+cosmicwanderer7@users.noreply.github.com>
2022-01-24 15:23:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = {
   images: {
     domains: ["i.imgur.com" , "www.google.com"],
   },
