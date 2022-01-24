0482bb557418ef85ee05b135969e741f4326a793
Prithvijeet <65147258+cosmicwanderer7@users.noreply.github.com>
2022-01-24 14:56:17 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
 
          <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
              <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2 font-Ubuntu">
-          <img src="github-brands.png" class="h-5  px-1 "></img> <a href="https://github.com/cosmicwanderer7"> Github.com/cosmicwanderer7 </a>
+          <img src="github-brands.png" className="h-5  px-1 "></img> <a href="https://github.com/cosmicwanderer7"> Github.com/cosmicwanderer7 </a>
              </div>
   
              <div className="flex justify-center text-sm space-x-5 whitespace-nowrap md:justify-self-start">
